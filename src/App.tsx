b2660d5ec8587a31d3b1abb0aa618d86685de5eb
camille l <camilleschmittlaroche@gmail.com>
2025-10-24 11:50:27 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+    return (
+        <div className="min-h-screen grid place-items-center">
+            <h1 className="text-3xl font-bold">Hello Tailwind + React + Vite 🚀</h1>
+        </div>
+    )
+}
+
+export default App
